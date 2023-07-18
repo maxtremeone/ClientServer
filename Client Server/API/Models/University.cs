b63b2823aad72c321385bc027139f99d02c860a4
unknown <ahlulhalliwal09@gmail.com>
2023-07-18 09:10:13 +0700

    Create University.cs

---
+++ Client Server/API/Models/University.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace API.Models;
+public class University
+{
+    public Guid Guid { get; set; } //uniq
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedDate { get; set; } 
+    public DateTime ModifiedDate { get; set;}
+}
+
