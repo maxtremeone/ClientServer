5845169e8930d67e50e3d9d6c5392fad2cf2b132
unknown <ahlulhalliwal09@gmail.com>
2023-07-19 21:20:51 +0700

    Create 20230718100107_Cardinality.cs

---
+++ Client Server/API/Migrations/20230718100107_Cardinality.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class Cardinality : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_tr_bookings_employee_guid",
+                table: "tb_tr_bookings",
+                column: "employee_guid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_tr_bookings_room_guid",
+                table: "tb_tr_bookings",
+                column: "room_guid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_m_educations_university_guid",
+                table: "tb_m_educations",
+                column: "university_guid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tb_m_educations_tb_m_employee_guid",
+                table: "tb_m_educations",
+                column: "guid",
+                principalTable: "tb_m_employee",
+                principalColumn: "guid",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tb_m_educations_tb_m_universities_university_guid",
+                table: "tb_m_educations",
+                column: "university_guid",
+                principalTable: "tb_m_universities",
+                principalColumn: "guid",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tb_m_employee_tb_m_accounts_guid",
+                table: "tb_m_employee",
+                column: "guid",
+                principalTable: "tb_m_accounts",
+                principalColumn: "guid",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tb_tr_account_roles_tb_m_accounts_guid",
+                table: "tb_tr_account_roles",
+                column: "guid",
+                principalTable: "tb_m_accounts",
+                principalColumn: "guid",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tb_tr_account_roles_tb_m_roles_guid",
+                table: "tb_tr_account_roles",
+                column: "guid",
+                principalTable: "tb_m_roles",
+                principalColumn: "guid",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tb_tr_bookings_tb_m_employee_employee_guid",
+                table: "tb_tr_bookings",
+                column: "employee_guid",
+                principalTable: "tb_m_employee",
+                principalColumn: "guid",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tb_tr_bookings_tb_m_rooms_room_guid",
+                table: "tb_tr_bookings",
+                column: "room_guid",
+                principalTable: "tb_m_rooms",
+                principalColumn: "guid",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tb_m_educations_tb_m_employee_guid",
+                table: "tb_m_educations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_tb_m_educations_tb_m_universities_university_guid",
+                table: "tb_m_educations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_tb_m_employee_tb_m_accounts_guid",
+                table: "tb_m_employee");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_tb_tr_account_roles_tb_m_accounts_guid",
+                table: "tb_tr_account_roles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_tb_tr_account_roles_tb_m_roles_guid",
+                table: "tb_tr_account_roles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_tb_tr_bookings_tb_m_employee_employee_guid",
+                table: "tb_tr_bookings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_tb_tr_bookings_tb_m_rooms_room_guid",
+                table: "tb_tr_bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_tb_tr_bookings_employee_guid",
+                table: "tb_tr_bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_tb_tr_bookings_room_guid",
+                table: "tb_tr_bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_tb_m_educations_university_guid",
+                table: "tb_m_educations");
+        }
+    }
+}
