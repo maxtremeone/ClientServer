133a3fd54f820c592b596454d4c236e90d4c60fd
unknown <ahlulhalliwal09@gmail.com>
2023-07-20 09:01:44 +0700

    Update AccountRole.cs

--- Client Server/API/Models/AccountRole.cs
+++ Client Server/API/Models/AccountRole.cs
@@ -1,11 +1,17 @@
-﻿namespace API.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
-public class AccountRole
+namespace API.Models
 {
-    public Guid Guid { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public DateTime ModifiedDate { get; set; }
-    public Guid AccountGuid { get; set; }
-    public Guid RoleGuid { get; set; }
-}
+    [Table("tb_tr_account_roles")]
+    public class AccountRole : BaseEntity
+    {
+        [Column("account_guid")]
+        public Guid AccountGuid { get; set; }
+        [Column("role_guid")]
+        public Guid RoleGuid { get; set; }
 
+        //Cardinality
+        public Role? Role { get; set; }
+        public Account? Account { get; set; }
+    }
+}
