0f205fa8e16c239aaadb1a030e14572f1c982e1e
unknown <ahlulhalliwal09@gmail.com>
2023-07-20 09:02:30 +0700

    Update Program.cs

--- Client Server/API/Program.cs
+++ Client Server/API/Program.cs
@@ -1,8 +1,14 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+//Add DBContext to the container
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BookingDbContext>(option => option.UseSqlServer(connection));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
