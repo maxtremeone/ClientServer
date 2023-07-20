a44f4d56a8f4c7eb946d4c72f45928259db20d15
unknown <ahlulhalliwal09@gmail.com>
2023-07-20 09:02:18 +0700

    Update Employee.cs

--- Client Server/API/Models/Employee.cs
+++ Client Server/API/Models/Employee.cs
@@ -1,16 +1,32 @@
-﻿namespace API.Models;
-public class Employee
-{
-    public Guid Guid { get; set; }
-    public string Nik { get; set; }
-    public string FirstName { get; set; }
-    public string? LastName { get; set; }
-    public DateTime BirthDate { get; set; }
-    public int Gender { get; set; }
-    public DateTime HiringDate { get; set; }
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public DateTime ModifiedDate { get; set;}
-}
+﻿using API.Utilities.Enums;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace API.Models
+{
+    [Table("tb_m_employees")]
+    public class Employee : BaseEntity
+    {
+        [Column("nik", TypeName = "nchar(6)")]
+        public string Nik { get; set; }
+        [Column("first_name", TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; }
+        [Column("last_name", TypeName = "nvarchar(100)")]
+        public string? LastName { get; set; }
+        [Column("birth_date")]
+        public DateTime BirthDate { get; set; }
+        [Column("gender")]
+        public GenderLevel Gender { get; set; }
+        [Column("hiring_date")]
+        public DateTime HiringDate { get; set; }
+        [Column("email", TypeName = "nvarchar(100)")]
+        public string Email { get; set; }
+        [Column("phone_number", TypeName = "nvarchar(20)")]
+        public string PhoneNumber { get; set; }
+
+        //Cardinality
+        public Education? Education { get; set; }
+        public ICollection<Booking>? Bookings { get; set; }
+        public Account? Account { get; set; }
+    }
+}
