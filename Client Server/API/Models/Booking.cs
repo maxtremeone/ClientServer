c9a0ebc45f154bfb7d16477f3c36e98aedbcbdab
unknown <ahlulhalliwal09@gmail.com>
2023-07-20 09:01:55 +0700

    Update Booking.cs

--- Client Server/API/Models/Booking.cs
+++ Client Server/API/Models/Booking.cs
@@ -1,14 +1,27 @@
-﻿namespace API.Models;
+﻿using API.Utilities.Enums;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
 
-public class Booking 
+namespace API.Models
 {
-    public Guid Guid { get; set; }
-    public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
-    public int Status { get; set; }
-    public string Remarks { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public DateTime ModifiedDate { get; set;}
-    public Guid RoomGuid { get; set; }
-    public Guid EmployeeGuid { get; set; }
+    [Table("tb_tr_bookings")]
+    public class Booking : BaseEntity
+    {
+        [Column("start_date")]
+        public DateTime StartDate { get; set; }
+        [Column("end_date")]
+        public DateTime EndDate { get; set; }
+        [Column("status")]
+        public StatusLevel Status { get; set; }
+        [Column("remarks")]
+        public string Remarks { get; set; }
+        [Column("room_guid")]
+        public Guid RoomGuid { get; set; }
+        [Column("employee_guid")]
+        public Guid EmployeeGuid { get; set; }
+
+        //Cardinality
+        public Room? Room { get; set; }
+        public Employee? Employee { get; set; }
+    }
 }
