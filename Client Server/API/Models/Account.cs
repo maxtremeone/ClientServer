01f5b4faccdf47e6268e85483e8dda4b661979e5
unknown <ahlulhalliwal09@gmail.com>
2023-07-20 09:01:41 +0700

    Update Account.cs

--- Client Server/API/Models/Account.cs
+++ Client Server/API/Models/Account.cs
@@ -1,14 +1,21 @@
-﻿namespace API.Models;
-public class Account 
-{
-    public Guid Guid { get; set; }
-    public string Password { get; set; }
-    public int Otp { get; set; }
-    public bool IsUsed { get; set; }
-    public DateTime ExpiredDate { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public DateTime ModifiedDate { get; set; }
-}
-    
-    
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
+namespace API.Models
+{
+    [Table("tb_m_accounts")]
+    public class Account : BaseEntity
+    {
+        [Column("password")]
+        public string Password { get; set; }
+        [Column("otp")]
+        public int Otp { get; set; }
+        [Column("is_used")]
+        public bool IsUsed { get; set; }
+        [Column("expired_time")]
+        public DateTime ExpiredTime { get; set; }
+
+        //Cardinality
+        public ICollection<AccountRole>? AccountRoles { get; set; }
+        public Employee? Employee { get; set; }
+    }
+}
